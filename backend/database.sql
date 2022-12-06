ae1b375eafcab6baed47c86fa9448e26ad490d5a
SIMON BULLADO <s_bullado51@hotmail.com>
2022-12-06 17:24:13 +0100

    article_in_database

--- backend/database.sql
+++ backend/database.sql
@@ -4,3 +4,13 @@ CREATE TABLE item (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO item (title) VALUES ('Stuff'), ('Doodads');
+
+create table article (
+  id int(11) unsigned primary key not null auto_increment,
+  title varchar(100) not null,
+  content varchar(280) NOT NULL
+) engine=InnoDB default charset=latin1;
+
+insert into article (title, content) VALUES
+  ('Lorem ipsum 1', 'Lorem content 1'),
+  ('Lorem ipsum 2', 'Lorem content 2');
