b27257106c4dd371acaa44dd756e56cd9fa63494
farid <faridhimeur@outlook.fr>
2022-12-06 11:41:07 +0100

    add icon navbar and edited css navbar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,22 +1,25 @@
 import "./NavBar.css";
+import home from "../assets/home.png";
+import menu from "../assets/menu.png";
+import user from "../assets/user.png";
 
 function NavBar() {
   return (
     <nav className="navbar">
       <ul className="list">
-        <li>
+        <li className="listbutton">
           <button className="button" type="button">
-            Home
+            <img className="imagenavbar" src={home} alt="home" />
           </button>
         </li>
-        <li>
+        <li className="listbutton">
           <button className="button" type="button">
-            Menu
+            <img className="imagenavbar" src={menu} alt="home" />
           </button>
         </li>
-        <li>
+        <li className="listbutton">
           <button className="button" type="button">
-            Profil
+            <img className="imagenavbar" src={user} alt="home" />
           </button>
         </li>
       </ul>
