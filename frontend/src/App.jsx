59d6dddb3c049c4d635d566ef05257c8fba4b262
farid <faridhimeur@outlook.fr>
2022-12-07 15:16:16 +0100

    add communicate plan with component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-
+import NavBar from "./components/NavBar";
 import Home from "./pages/Home";
-
-import "./App.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <NavBar />
+    </>
   );
 }
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,33 +1,7 @@
-import NavBar from "../components/NavBar";
+import "../components/NavBar.css";
+import NewsFeed from "../components/NewsFeed";
 
 function Home() {
-  return (
-    <>
-      <header className="App-header">
-        <h1>Titre du Blog !</h1>
-      </header>
-      <NavBar />
-      <main>
-        <article>
-          <h1>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
-            laboriosam quod corrupti fuga hic! Harum ducimus quas odio esse.
-            Eligendi numquam suscipit laborum atque ad itaque libero reiciendis
-            omnis voluptate.
-          </h1>
-        </article>
-        <article>
-          <h1>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
-            laboriosam quod corrupti fuga hic! Harum ducimus quas odio esse.
-            Eligendi numquam suscipit laborum atque ad itaque libero reiciendis
-            omnis voluptate.
-          </h1>
-        </article>
-      </main>
-
-      <footer>Ici le Footer</footer>
-    </>
-  );
+  return <NewsFeed />;
 }
 export default Home;
