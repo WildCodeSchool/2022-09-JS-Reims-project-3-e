3c1c40ad620b3aa7e1ec5a33417e71f44f35c70f
SIMON BULLADO <s_bullado51@hotmail.com>
2022-12-07 04:30:44 +0100

    fetch_api_first_demo

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,26 +1,38 @@
+import { useEffect, useState } from "react";
 import NavBar from "../components/NavBar";
 
-function Home() {
+export default function Home() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${import.meta.env.VITE_BACKEND_URL ?? "http://localhost/5000"}/articles`
+    )
+      .then((response) => response.json())
+      .then((data) => setArticles(data));
+  }, []);
+
   return (
     <>
       <header className="App-header">
-        <h1>Titre du Blog !</h1>
+        <h1>Title !</h1>
       </header>
       <NavBar />
       <main>
         <article>
-          <h1>Titre</h1>
+          <h1>Title</h1>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
           laboriosam quod corrupti fuga hic! Harum ducimus quas odio esse.
           Eligendi numquam suscipit laborum atque ad itaque libero reiciendis
           omnis voluptate.
         </article>
         <article>
-          <h1>Titre</h1>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
-          laboriosam quod corrupti fuga hic! Harum ducimus quas odio esse.
-          Eligendi numquam suscipit laborum atque ad itaque libero reiciendis
-          omnis voluptate.
+          <h1>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Adipisci
+            laboriosam quod corrupti fuga hic! Harum ducimus quas odio esse.
+            Eligendi numquam suscipit laborum atque ad itaque libero reiciendis
+            omnis voluptate.
+          </h1>
         </article>
       </main>
 
@@ -28,4 +40,3 @@ function Home() {
     </>
   );
 }
-export default Home;
