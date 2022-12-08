d8b1b193efe038724d0d4757fe359cdfb8be43ca
farid <faridhimeur@outlook.fr>
2022-12-08 15:29:37 +0100

    add css of feedback and avatar profil and name for design

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -1,12 +1,17 @@
 import "./NewsFeed.css";
+import React from "react";
 
 function NewsFeed() {
   return (
     <section className="content">
       <article className="article">
         <div className="profil">
-          <img src="" alt="Avatar Profil" />
-          <p>Name of User</p>
+          <img
+            className="imageprofil"
+            src="https://media-exp1.licdn.com/dms/image/D4E03AQEKkzhQP5r5lg/profile-displayphoto-shrink_800_800/0/1670411909070?e=1675900800&v=beta&t=QJDauJPYN2CGqSL-Ky0sPjnN9w09IyXjWoivLOlFQKg"
+            alt="Avatar Profil"
+          />
+          <p>Simon B</p>
         </div>
         <h1>CHSCT</h1>
         <p>
@@ -18,14 +23,16 @@ function NewsFeed() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <ul className="listfeedback">
-          <li className="feedback">Like</li>
-        </ul>
+        <div className="isFavorite"> </div>
       </article>
       <article className="article">
         <div className="profil">
-          <img src="" alt="Avatar Profil" />
-          <p>Name of User</p>
+          <img
+            className="imageprofil"
+            src="https://media-exp1.licdn.com/dms/image/D4E35AQF5X3q2dvbUjA/profile-framedphoto-shrink_800_800/0/1663860060762?e=1671112800&v=beta&t=509_YHyLf-PeRCdSruGq95noIi1rdbNVvReutK60Gqw"
+            alt="Avatar Profil"
+          />
+          <p>Olivier N</p>
         </div>
         <h1>CHSCT</h1>
         <p>
@@ -37,14 +44,16 @@ function NewsFeed() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <ul className="listfeedback">
-          <li className="feedback">Like</li>
-        </ul>
+        <div className="isFavorite"> </div>
       </article>
       <article className="article">
         <div className="profil">
-          <img src="" alt="Avatar Profil" />
-          <p>Name of User</p>
+          <img
+            className="imageprofil"
+            src="https://media-exp1.licdn.com/dms/image/D4E03AQGfq_Zm3TiWog/profile-displayphoto-shrink_800_800/0/1670508543662?e=1675900800&v=beta&t=YfaQytlNMmOVu4Cs1SLDEcK3ZsOrZVYllK-T5AQtUUo"
+            alt="Avatar Profil"
+          />
+          <p>Farid H</p>
         </div>
         <h1>CHSCT</h1>
         <p>
@@ -56,14 +65,16 @@ function NewsFeed() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <ul className="listfeedback">
-          <li className="feedback">Like</li>
-        </ul>
+        <div className="notFavorite"> </div>
       </article>
       <article className="article">
         <div className="profil">
-          <img src="" alt="Avatar Profil" />
-          <p>Name of User</p>
+          <img
+            className="imageprofil"
+            src="https://media-exp1.licdn.com/dms/image/D4E35AQFi4xZrBf0K2w/profile-framedphoto-shrink_400_400/0/1667069647405?e=1671112800&v=beta&t=Lh0UxsrLiiPbKq_9fYrPsTIAAb39hRrHjVniOmwGrgo"
+            alt="Avatar Profil"
+          />
+          <p>Adnan B</p>
         </div>
         <h1>CHSCT</h1>
         <p>
@@ -75,9 +86,28 @@ function NewsFeed() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <ul className="listfeedback">
-          <li className="feedback">Like</li>
-        </ul>
+        <div className="isFavorite"> </div>
+      </article>
+      <article className="article">
+        <div className="profil">
+          <img
+            className="imageprofil"
+            src="https://media-exp1.licdn.com/dms/image/D4E03AQFh_00lRkCTnw/profile-displayphoto-shrink_800_800/0/1665997512112?e=1675900800&v=beta&t=jnIBPdTukdIYcP-qLFEh7g6tXVNAklT36xgj8XfQC1g"
+            alt="Avatar Profil"
+          />
+          <p>Pierre-Yves G</p>
+        </div>
+        <h1>CHSCT</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <div className="notFavorite"> </div>
       </article>
     </section>
   );
