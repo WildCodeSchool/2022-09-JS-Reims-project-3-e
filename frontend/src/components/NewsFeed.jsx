3b0e47d8decf1f5ae9eaa3bf556c833c76680cef
farid <faridhimeur@outlook.fr>
2022-12-08 13:45:34 +0100

    css homepage

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -63,6 +63,26 @@ function NewsFeed() {
           <li>Share</li>
         </ul>
       </article>
+      <article>
+        <div>
+          <img src="" alt="Avatar Profil" />
+          <p>Name of User</p>
+        </div>
+        <h1>CHSCT</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+        <ul>
+          <li>Like</li>
+          <li>Share</li>
+        </ul>
+      </article>
     </section>
   );
 }
