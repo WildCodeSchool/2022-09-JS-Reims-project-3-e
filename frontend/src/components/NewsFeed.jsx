3416f5552d2d72db73664eb542390b205e7b0af7
SIMON BULLADO <s_bullado51@hotmail.com>
2022-12-14 10:15:39 +0100

    refactored for PR

--- frontend/src/components/NewsFeed.jsx
+++ frontend/src/components/NewsFeed.jsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
+import CommunicationPlanCard from "./CommunicationPlanCard";
 import "./NewsFeed.css";
-import PlanComs from "./PlanComs";
 
 function NewsFeed() {
-  return <PlanComs />;
+  const [communicationPlans, setCommunicationPlans] = useState([]);
+  useEffect(() => {
+    fetch(
+      `${import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"}/articles`
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        setCommunicationPlans(response);
+      });
+  }, []);
+  return (
+    <div>
+      {communicationPlans.map((communicationPlan) => {
+        return (
+          <CommunicationPlanCard
+            title={communicationPlan.title}
+            content={communicationPlan.content}
+            key={communicationPlan.id}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default NewsFeed;
