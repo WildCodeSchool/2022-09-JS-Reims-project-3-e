0b5534c36ab3c349f9a18c3aa4b0eb430b824ed3
Adnan <adbak.dev@gmail.com>
2022-12-14 15:42:09 +0100

    added ui components

---
+++ frontend/src/UI/Card.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import classes from "./Card.module.css";
+
+function Card({ classNames, children }) {
+  return <div className={`${classes.card} ${classNames}`}>{children}</div>;
+}
+
+export default Card;
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
+  classNames: PropTypes.string,
+};
+
+
+Card.defaultProps = {
+  classNames: "",
+};
+
