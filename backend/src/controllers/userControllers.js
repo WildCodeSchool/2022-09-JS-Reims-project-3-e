313956a09bc9ec35d75743f30ed622b29697f87e
Adnan <adbak.dev@gmail.com>
2022-12-19 09:33:12 +0100

    added user login handler with argon and token

--- backend/src/controllers/userControllers.js
+++ backend/src/controllers/userControllers.js
@@ -1,3 +1,5 @@
+const argon2 = require("argon2");
+const jwt = require("jsonwebtoken");
 const models = require("../models");
 
 const browse = (req, res) => {
@@ -12,6 +14,194 @@ const browse = (req, res) => {
     });
 };
 
+const read = (req, res) => {
+  models.users
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const users = req.body;
+
+  // TODO validations (length, format...)
+
+  users.id = parseInt(req.params.id, 10);
+
+  models.users
+    .update(users)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const user = req.body;
+
+  // TODO validations (length, format...)
+
+  models.users
+    .insert(user)
+    .then(([result]) => {
+      res.location(`/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.users
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const [users] = await models.users.verifyEmailAndPasswordAndNext(email);
+    if (users[0] == null) {
+      res.sendStatus(401);
+    } else {
+      const validPassword = await argon2.verify(
+        users[0].hashedPassword,
+        password
+      );
+      if (validPassword) {
+        const payload = {
+          id: users[0].id,
+          email: users[0].email,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        res.send({ token });
+      } else {
+        res.sendStatus(401);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+};
+
+const verifyToken = (req, res, next) => {
+  try {
+    const authorizationHeader = req.get("Authorization");
+    if (authorizationHeader == null) {
+      throw new Error("Authorization header is missing");
+    }
+
+    const [type, token] = authorizationHeader.split(" ");
+
+    if (type !== "Bearer") {
+      throw new Error("Authorization header has not the 'Bearer' type");
+    }
+
+    req.payload = jwt.verify(token, process.env.JWT_SECRET);
+
+    next();
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(401);
+  }
+};
+
 module.exports = {
   browse,
+  read,
+  edit,
+  add,
+  destroy,
+  login,
+  verifyToken,
 };
+
+/*
+const login = (req, res, next) => {
+  const { email } = req.body;
+
+  models.users
+    .verifyEmailAndPasswordAndNext(email)
+    .then(([users]) => {
+      if (users[0] == null) {
+        res.sendStatus(401);
+      } else {
+        const payload = {
+          id: users[0].id,
+          email: users[0].email,
+        };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+          expiresIn: "1h",
+        });
+        res.send({ token });
+        next();
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+*/
+
+/*
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  models.users.verifyEmailAndPasswordAndNext(email).then(([rows]) => {
+    if (rows.length === 0) {
+      res.sendStatus(401);
+    } else {
+      const user = rows[0];
+      argon2
+        .verify(user.hashedPassword, password)
+        .then((match) => {
+          if (match) {
+            const payload = { id: user.id };
+            const token = jwt.sign(payload, process.env.JWT_SECRET, {
+              expiresIn: "1h",
+            });
+            res.send({ token });
+            next();
+          } else {
+            res.sendStatus(401);
+          }
+        })
+        .catch((err) => {
+          console.error(err);
+          res.sendStatus(500);
+        });
+    }
+  });
+}; */
