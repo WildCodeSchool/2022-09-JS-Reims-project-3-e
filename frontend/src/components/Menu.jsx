177d9b697e75a5d98ec4bc1e0561118495eb6f22
farid <faridhimeur@outlook.fr>
2022-12-20 10:10:14 +0100

    add categories and image

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,4 +1,10 @@
 import "./Menu.css";
+import chat from "../assets/chat.jpg";
+import agence from "../assets/agence.jpg";
+import clients from "../assets/clients.jpg";
+import enedis from "../assets/enedis.png";
+import metier from "../assets/metier.png";
+import prevention from "../assets/prevention.jpeg";
 
 function Menu() {
   return (
@@ -6,32 +12,32 @@ function Menu() {
       <ul>
         <li>
           <button className="buttonwidget" type="button">
-            <img src="" alt="profil" />
+            <img src={enedis} alt="enedis" />
           </button>
         </li>
         <li>
           <button className="buttonwidget" type="button">
-            <img src="" alt="profil" />
+            <img src={agence} alt="communication" />
           </button>
         </li>
         <li>
           <button className="buttonwidget" type="button">
-            <img src="" alt="profil" />
+            <img src={prevention} alt="job" />
           </button>
         </li>
         <li>
           <button className="buttonwidget" type="button">
-            <img src="" alt="profil" />
+            <img src={metier} alt="prevention" />
           </button>
         </li>
         <li>
           <button className="buttonwidget" type="button">
-            <img src="" alt="profil" />
+            <img src={clients} alt="customer" />
           </button>
         </li>
         <li>
           <button className="buttonwidget" type="button">
-            <img src="" alt="profil" />
+            <img src={chat} alt="tchat" />
           </button>
         </li>
       </ul>
