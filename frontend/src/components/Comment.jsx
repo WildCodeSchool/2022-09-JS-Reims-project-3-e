8953fa9778d715755a6e42e70576ec44f1b49d63
farid <faridhimeur@outlook.fr>
2023-01-05 14:06:47 +0100

    finished css article

--- frontend/src/components/Comment.jsx
+++ frontend/src/components/Comment.jsx
@@ -1,6 +1,20 @@
 import "./Comment.css";
+import submit from "../assets/submit.png";
 
 function Comment() {
-  return <div className="comments"> CONTAINER of COMMENTS </div>;
+  return (
+    <div className="comments">
+      <input
+        className="textcomment"
+        forHtml="textcomment"
+        type="text"
+        size="40"
+        placeholder="Commenter"
+      />
+      <button type="submit" className="submit">
+        <img className="imgsubmit" src={submit} alt="imgsubmit" />
+      </button>
+    </div>
+  );
 }
 export default Comment;
