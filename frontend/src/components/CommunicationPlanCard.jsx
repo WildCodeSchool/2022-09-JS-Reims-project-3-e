f3a020b5527c65ce2bbaed3c9c4b9e40f9d7b77d
farid <faridhimeur@outlook.fr>
2023-01-06 10:40:04 +0100

    adapt css in the component CommunicationPlan

--- frontend/src/components/CommunicationPlanCard.jsx
+++ frontend/src/components/CommunicationPlanCard.jsx
@@ -1,6 +1,10 @@
 import PropTypes from "prop-types";
-
+import buttonedited from "../assets/buttonedited.png";
+import comment from "../assets/comment.png";
+import heart from "../assets/heart.png";
+import Comment from "./Comment";
 import Like from "./Like";
+import "./NewsFeed.css";
 
 export default function CommunicationPlanCard({
   title,
@@ -10,17 +14,56 @@ export default function CommunicationPlanCard({
   return (
     <article className="article">
       <div className="profil">
-        <img
-          className="imageprofil"
-          src="https://media-exp1.licdn.com/dms/image/D4E03AQEKkzhQP5r5lg/profile-displayphoto-shrink_800_800/0/1670411909070?e=1675900800&v=beta&t=QJDauJPYN2CGqSL-Ky0sPjnN9w09IyXjWoivLOlFQKg"
-          alt="Avatar Profil"
-        />
-        <p>John Doe</p>
+        <div className="userprofil">
+          <img
+            className="imageprofil"
+            src="https://media-exp1.licdn.com/dms/image/D4E03AQEKkzhQP5r5lg/profile-displayphoto-shrink_800_800/0/1670411909070?e=1675900800&v=beta&t=QJDauJPYN2CGqSL-Ky0sPjnN9w09IyXjWoivLOlFQKg"
+            alt="Avatar Profil"
+          />
+          <div className="nameuser">
+            <p>Simon B</p>
+            <div className="cityandcompany">
+              <p>Reims</p>
+              <p>WCS</p>
+            </div>
+          </div>
+        </div>
+        <div className="userparameter">
+          <p className="dateofpublish">5 jours</p>
+          <img
+            className="buttonedited"
+            src={buttonedited}
+            alt="icon edited and remove"
+          />
+        </div>
       </div>
-      <img className="imageurl" src={imageUrl} alt="imageurl" />
-      <h1>{title}</h1>
-      <p>{content}</p>
-      <Like isFavorite={false} />
+      <img
+        className="imagecontent"
+        src="https://remeng.rosselcdn.net/sites/default/files/dpistyles_v2/ena_16_9_extra_big/2018/12/10/node_20755/530170/public/2018/12/10/B9717886568Z.1_20181210094232_000%2BGERCIOLFT.1-0.jpg?itok=t6jVgxVZ1544431361"
+        alt="imagecontent"
+      />
+      <div className="contentofpublication">
+        <img className="imageurl" src={imageUrl} alt="imageurl" />
+        <h1 className="titlecontent">{title}</h1>
+        <p className="descriptioncontent">{content}</p>
+      </div>
+      <div className="buttonlikeandcomment">
+        <div className="likeandcount">
+          <p className="numberoflike"> 4 </p>
+          <img src={heart} className="countoflike" alt="countoflike" />
+        </div>
+        <div className="buttonlikeandcomments">
+          <div className="likes">
+            <Like isFavorite={false} />
+            <p className="styleoflike">J'aime</p>
+          </div>
+          <div className="comments">
+            <img className="comment" src={comment} alt="comment" />
+            <p className="styleofcomment">Je commente</p>
+          </div>
+        </div>
+      </div>
+      <Comment />
     </article>
   );
 }
